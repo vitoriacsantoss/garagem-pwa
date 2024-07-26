728c00d8866eb8e9aec995ee21b48c767333a645
vitoriacsantoss <santosviih360@gmail.com>
2024-07-26 20:48:45 -0300

    Configurando axios.js

---
+++ src/plugins/axios.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:19003/";
+
+axios.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('psg_auth_token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    },
+);
