1c0c51cdc4843111abd83bbea704f3966995f6b8
vitoriacsantoss <santosviih360@gmail.com>
2024-07-26 21:23:32 -0300

    Adicionando, editando e removendo categorias

--- src/stores/garagem.js
+++ src/stores/garagem.js
@@ -2,6 +2,11 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
 import acessoriosService from '@/services/acessorios'
+import veiculosService from '@/services/veiculos'
+import modelosService from '@/services/modelos'
+import categoriasService from '@/services/categorias'
+import coresService from '@/services/cores'
+import marcasService from '@/services/marcas'
 
 export const useGaragemStore = defineStore('garagem', () => {
   const veiculos = ref([])
@@ -11,80 +16,124 @@ export const useGaragemStore = defineStore('garagem', () => {
   const cores = ref([])
   const marcas = ref([])
 
-  async function buscarVeiculos() {}
+  async function buscarVeiculos() {
+    const data = await veiculosService.buscarVeiculos()
+    veiculos.value = data
+  }
 
-  async function editarVeiculo(veiculo) {}
+  async function editarVeiculo(veiculo) {
+    await veiculosService.editarVeiculo(veiculo)
+    await buscarVeiculos()
+  }
 
-  async function adicionarVeiculo(veiculo) {}
+  async function adicionarVeiculo(veiculo) {
+    await veiculosService.adicionarVeiculo(veiculo)
+    await buscarVeiculos()
+  }
 
-  async function removerVeiculo(id) {}
+  async function removerVeiculo(id) {
+    await veiculosService.removerVeiculo(id)
+    await buscarVeiculos()
+  }
 
   async function buscarModelos() {
+    const data = await modelosService.buscarModelos()
+    modelos.value = data
   }
 
   async function editarModelo(modelo) {
+    await modelosService.editarModelo(modelo)
+    await buscarModelos()
   }
 
   async function adicionarModelo(modelo) {
+    await modelosService.adicionarModelo(modelo)
+    await buscarModelos()
   }
 
   async function removerModelo(id) {
+    await modelosService.removerModelo(id)
+    await buscarModelos()
   }
 
   async function buscarAcessorios() {
     const data = await acessoriosService.buscarAcessorios()
-    acessorios.value = data;
+    acessorios.value = data
   }
 
   async function editarAcessorio(acessorio) {
-    await acessoriosService.editarAcessorio(acessorio);
-    await buscarAcessorios() 
+    await acessoriosService.editarAcessorio(acessorio)
+    await buscarAcessorios()
   }
 
   async function adicionarAcessorio(acessorio) {
-    await acessoriosService.adicionarAcessorio(acessorio);
+    await acessoriosService.adicionarAcessorio(acessorio)
     await buscarAcessorios()
   }
 
   async function removerAcessorio(id) {
-    await acessoriosService.removerAcesserio(id);
+    await acessoriosService.removerAcesserio(id)
     await buscarAcessorios()
   }
 
   async function buscarCategorias() {
+    const data = await categoriasService.buscarCategorias()
+    categorias.value = data
   }
 
   async function editarCategoria(categoria) {
+    await categoriasService.editarCategoria(categoria)
+    await buscarCategorias()
   }
 
   async function adicionarCategoria(categoria) {
+    await categoriasService.adicionarCategoria(categoria)
+    await buscarCategorias()
   }
 
   async function removerCategoria(id) {
+    await categoriasService.removerCategoria(id)
+    await buscarCategorias()
   }
 
   async function buscarCores() {
+    const data = await coresService.buscarCores()
+    cores.value = data
   }
 
   async function editarCor(cor) {
+    await coresService.editarCor(cor)
+    await buscarCores()
   }
 
   async function adicionarCor(cor) {
+    await coresService.adicionarCor(cor)
+    await buscarCores()
   }
 
   async function removerCor(id) {
+    await coresService.removerCor(id)
+    await buscarCores()
   }
 
   async function buscarMarcas() {
+    const data = await marcasService.buscarMarcas()
+    marcas.value = data
   }
 
   async function editarMarca(marca) {
+    await marcasService.editarMarca(marca)
+    await buscarMarcas()
   }
 
   async function adicionarMarca(marca) {
+    await marcasService.adicionarMarca(marca)
+    await buscarMarcas()
   }
 
   async function removerMarca(id) {
+    await marcasService.removerMarca(id)
+    await buscarMarcas()
   }
 
   return {
--- src/views/Categorias.vue
+++ src/views/Categorias.vue
@@ -1,4 +1,33 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref } from 'vue'
+import { useGaragemStore } from '@/stores/garagem'
+
+const storeGaragem = useGaragemStore()
+
+const defaultCategoria = {
+  id: null,
+  descricao: null
+}
+const categoria = ref({...defaultCategoria})
+
+async function salvar() {
+  if (categoria.value.id) {
+    await storeGaragem.editarCategoria(categoria.value);
+  } else {
+    await storeGaragem.adicionarCategoria(categoria.value)
+  }
+
+  categoria.value = {...defaultCategoria}
+}
+
+async function excluir(id) {
+  await storeGaragem.removerCategoria(id)
+}
+
+onMounted(async () => {
+  await storeGaragem.buscarCategorias()
+})
+</script>
 
 <template>
   <section>
@@ -7,12 +36,20 @@
     <div class="form">
       <div class="input">
         <p>Descricao</p>
-        <input type="text" />
+        <input type="text" v-model="categoria.descricao" />
       </div>
 
-      <button class="salvarButton">
-        Salvar
-      </button>
+      <button class="salvarButton" @click="salvar">Salvar</button>
+    </div>
+
+    <div class="list">
+      <div class="categoria" v-for="item in storeGaragem.categorias" :key="item.id">
+        <p @click="categoria = item">({{ item.id }}) - {{ item.descricao }}</p>
+
+        <span @click="excluir(item.id)">
+          <i class="mdi mdi-close"></i>
+        </span>
+      </div>
     </div>
   </section>
 </template>
@@ -54,13 +91,40 @@ section {
 }
 
 .salvarButton {
-    background-color: #007BFF;
-    color: white;              
-    border: none;             
-    border-radius: 4px;      
-    padding: 10px 20px;        
-    font-size: 16px;         
-    cursor: pointer;      
-    display: inline-block;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  display: inline-block;
+}
+
+.list {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.categoria {
+  display: flex;
+  gap: 10px;
+  background-color: #007bff50;
+  padding: 5px 10px;
+  border-radius: 5px;
+  overflow: hidden;
+}
+
+.categoria p {
+  min-width: 140px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  cursor: pointer;
+}
+
+.categoria span {
+  cursor: pointer;
 }
 </style>
